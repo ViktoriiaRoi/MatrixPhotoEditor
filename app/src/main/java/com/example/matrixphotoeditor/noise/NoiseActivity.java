601251a11ff8792b83008b82dad4999a3ee44234
bohdaholas <bohdan.ivashko@ucu.edu.ua>
2022-05-26 23:43:47 +0300

    noise

--- app/src/main/java/com/example/matrixphotoeditor/noise/NoiseActivity.java
+++ app/src/main/java/com/example/matrixphotoeditor/noise/NoiseActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -17,6 +18,8 @@ import android.widget.SeekBar;
 
 import com.example.matrixphotoeditor.MatrixImage;
 import com.example.matrixphotoeditor.R;
+
+import java.util.Random;
 
 public class NoiseActivity extends AppCompatActivity {
     static final String BITMAP_ARRAY = "Bitmap";
@@ -55,7 +58,32 @@ public class NoiseActivity extends AppCompatActivity {
     }
 
     private void noiseImage(int n) {
-        //TODO
+        int width = initialBitmap.getWidth();
+        int height = initialBitmap.getHeight();
+
+        Random rand = new Random();
+
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                int []rgb = new int[3];
+                rgb[0] = Color.red(initialBitmap.getPixel(x, y));
+                rgb[1] = Color.green(initialBitmap.getPixel(x, y));
+                rgb[2] = Color.blue(initialBitmap.getPixel(x, y));
+                // sigma 10 * n
+                double noise = rand.nextGaussian() * 10 * n;
+                for (int i = 0; i < rgb.length; i++) {
+                    rgb[i] += noise;
+                    if (rgb[i] < 0) {
+                        rgb[i] = 0;
+                    }
+                    if (rgb[i] > 255) {
+                        rgb[i] = 255;
+                    }
+                }
+                resultBitmap.setPixel(x, y, Color.rgb(rgb[0], rgb[1], rgb[2]));
+            }
+        }
+        matrixImage.setBitmap(resultBitmap);
     }
 
     private void saveChanges() {
